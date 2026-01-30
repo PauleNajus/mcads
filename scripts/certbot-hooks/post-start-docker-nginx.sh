c1c78d901c28dfcff02f9a1e92423369016cc631
Paulius Bundza <bundzapaulius@gmail.com>
2026-01-30 09:04:46 +0000

    Cleanup of unused files.

--- scripts/certbot-hooks/post-start-docker-nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# After Certbot finishes (success or failure), ensure the Nginx container is running.
-
-cd /home/debian/mcads
-
-sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d nginx
-
